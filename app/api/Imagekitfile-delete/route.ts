12e5aa0b4a9dd2be81ac71c61b815f2673de07d6
unknown <gehlotmitesh63@gmail.com>
2025-12-03 23:42:18 +0530

    fix: imagekit empty route & package.json

--- app/api/Imagekitfile-delete/route.ts
+++ app/api/Imagekitfile-delete/route.ts
@@ -1,23 +1,5 @@
-// import { NextRequest, NextResponse } from "next/server";
-// import { imagekit } from "@/lib/imagekit";
-// import { auth, currentUser } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server'
 
-// export async  function DELETE(req:NextRequest){
-// try {
-//     const {fileId}=await req.json();
-//     if(!fileId){
-//         return NextResponse.json({success:false,error:"fileId is neccessary"},{status:400})
-//     }
-//    const currentUser=await (req);
-//    if (!currentUser || typeof currentUser !== "object" || !("id" in currentUser)) {
-//     return NextResponse.json({ success: false, error: "Unauthorized request! Please try again later." }, { status: 401 });
-// }
-
-//     const response = await imagekit.deleteFile(fileId);
-//     return NextResponse.json({success:true,message:"File Deleted SuccessFully"},{status:200})
-
-// } catch (error) {
-//     console.log("File Not Deleted: ",error)
-//     return NextResponse.json({success:false,error:"Something went wrong!"},{status:500})
-// }
-// }
+export async function POST(req: Request) {
+  return NextResponse.json({ ok: true })
+}
